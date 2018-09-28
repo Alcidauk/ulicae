dc1ee07543d0eff054f45c0ae3ec5066135384c2
alcidauk <pir.r25@gmail.com>
2018-09-28 13:47:47 +0200

    Extract toolbar, and customize it

---
+++ src/components/cv.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container fluid>
+       <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped=true
+      v-model="drawer"
+      enable-resize-watcher
+    >
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Cv',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      miniVariant: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/toolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-toolbar color="lime darken-3">
+      <v-toolbar-title v-text="title" class="lime--text text--lighten-5 toolbar-title"/>
+      <v-toolbar-items justify-space-around>
+        <v-btn flat color="lime lighten-5"><v-icon left color="green lighten-3">mdi-home-outline</v-icon>Home</v-btn>
+        <v-btn flat color="lime lighten-5"><v-icon left color="light-green lighten-3">mdi-account-card-details</v-icon>CV</v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      title: "Ulicae",
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.toolbar-title {
+  margin-right: 25px;
+}
+</style>

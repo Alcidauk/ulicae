a629a99459cf84833fbc5735501eeaa457751204
Alcidauk <pierre@ulica.fr>
2020-04-13 18:00:34 +0200

    Center home

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,20 +1,24 @@
   <template>
-  <v-layout wrap fill-height child-flex secondary lighten-1>
-    <v-card class="ma-5">
-        <v-flex>
-          <v-card-title>
-            <h1>Bienvenue</h1>
-          </v-card-title>
-          <v-card-text>
-          <div class="align-center">Je suis Pierre Rognon, développeur informatique depuis quelques années maintenant.</div> 
-          <p>Vous trouverez ici <a href="/cv">CV</a>, <a href="/notes">ressources</a> et autres informations que je souhaite partager.</p>
-          <br/>
-          <p>Bonne visite !</p>
-          </v-card-text>
-        </v-flex>
+    <v-layout fill-height secondary lighten-1>
+      <v-container>
+        <v-card class="pa-5">
+          <h1 align="center" justify="center">Bienvenue</h1>
+
+          <div class="mt-5" align="center" justify="center">
+            Je suis Pierre Rognon, développeur informatique depuis quelques années maintenant.
+          </div>
+
+          <div class="mt-1 mb-5" align="center" justify="center">
+            Vous trouverez ici <a href="/cv">CV</a>, <a href="/notes">ressources</a> et autres informations que je souhaite partager.
+          </div>
+          
+          <div align="center" justify="center">
+            Bonne visite !
+          </div>   
+        </v-card>
+      </v-container>
+    </v-layout>
       
-    </v-card>
-  </v-layout>
   </template>
 
   <script>

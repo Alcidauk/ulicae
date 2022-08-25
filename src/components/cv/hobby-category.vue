be5022acd92e0ab070d62b99734914189e2a69a9
Alcidauk <pierre@ulica.fr>
2022-08-25 17:12:43 +0200

    CV: modify hobbies. Add Cinelog.

--- src/components/cv/hobby-category.vue
+++ src/components/cv/hobby-category.vue
@@ -5,6 +5,12 @@
                 <h1 class="mb-2 page-title">Centres d'intérêts</h1>
                 <v-layout column>
                   <HobbyStep 
+                    title="Développement mobile de l'application Cinélog" 
+                    description="Développement fonctionnel et graphique d'une application de revue de films et séries" 
+                    subDescription="Crédits: Kerstin Maur" 
+                    imgsrc="cinelog_large.png"
+                    imgposition="left 0% top 50%"/>
+                  <HobbyStep 
                     title="Supermarché coopératif" 
                     description="implication dans les groupes d'approvisionnement aux 400 Coop (Paris 20ème)"
                     subDescription="Crédits photographies: Arnaud CAILLOU / L’Oeil Témoin" 
@@ -15,12 +21,6 @@
                     description="intérêt pour les techniques de barista" 
                     subDescription="Crédits photographiques: eliasfalla / pixabay.com" 
                     imgsrc="coffee.jpg"
-                    imgposition="left 0% top 30%"/>
-                  <HobbyStep 
-                    title="Jardinage" 
-                    description="pratique du jardinage en temps libre" 
-                    subDescription="Crédits photographiques: ulleo / pixabay.com" 
-                    imgsrc="jardin.jpg"
                     imgposition="left 0% top 30%"/>
                 </v-layout>
             </v-container>
